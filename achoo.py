c56adb8861e97130fc32430ab81f7fc9fea34268
shoheiyokoyama <shohei.yok0602@gmail.com>
2018-12-11 00:51:38 +0900

    Update achoo.py

--- achoo.py
+++ achoo.py
@@ -1,7 +1,9 @@
 import lldb
 
+# 🤧
 def achoo(debugger, command, result, internal_dict):
     print "Bless you"
 
-def __lldb_init_module(debugger, dict):
+def __lldb_init_module(debugger, internal_dict):
+    # command script add -f filename.function_name command_name
     debugger.HandleCommand("command script add -f achoo.achoo achoo")
